479cbc0eadd7c648a23691bc3850ebae6f57aa72
Dimitar Ivanov <dimitar.ivanov@a1.bg>
2023-04-30 14:47:10 +0300

    authentication menu frontend

--- client/pages/components/Header.tsx
+++ client/pages/components/Header.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthMenu from "./AuthMenu";
 
 const Header = ({}) => {
+  const [isAuthMenuOpen, setAuthMenuOpen] = useState<Boolean>(false);
+
+  const closeAuth = () => setAuthMenuOpen(false);
+
   return (
-    <div className="flex justify-between">
-      <div>CineRooms</div>
-      <div>
-        <input type="text" placeholder="Find movie..." />
-        <button>Search</button>
-        <button>Login</button>
-      </div>
+    <div className=" w-full">
+      {isAuthMenuOpen ? (
+        <AuthMenu closeAuth={closeAuth} />
+      ) : (
+        <div className="flex w-full justify-between">
+          <div>CineRooms</div>
+          <div>
+            <input type="text" placeholder="Find movie..." />
+            <button>Search</button>
+            <button onClick={() => setAuthMenuOpen(true)}>Login</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
